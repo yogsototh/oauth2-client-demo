be139403bbdad6838d304c513ca5dbee4915e608
Yann Esposito (Yogsototh) <yann.esposito@gmail.com>
2018-06-04 21:54:38 +0200

    use code

--- infos.js
+++ infos.js
@@ -2,10 +2,10 @@ var oauthURLPrefix="http://localhost:9001";
 var oauthServerUrl=oauthURLPrefix + "/iroh/oauth2/authorize";
 var oauthServerTokenUrl=oauthURLPrefix + "/iroh/oauth2/token";
 var resourceProviderTestEndpoint=oauthURLPrefix + "/iroh/iroh-ui-settings/whoami" ;
-var response_type="token";
+var response_type="code";
 var client_id="localtest";
 var client_password = "localpass";
-var redirect_uri="http://localhost:9999/implicit.html";
+var redirect_uri="http://localhost:9999/code.html";
 var scopes=[ "private-intel"
            , "ui-settings"
            //  , inexistant
