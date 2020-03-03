99a2b736401006df1e2e29425829a0285fded969
Leland Krych <lkrych@cisco.com>
2020-03-02 21:36:58 -0800

    move scripts to scripts folder

---
+++ scripts/test_access_token.sh
@@ -0,0 +1,5 @@
+curl -G https://visibility.amp.cisco.com/iroh/profile/whoami \
+    --header "Accept: application/json" \
+    --header "application/json; charset=UTF-8" \
+    --header "User-Agent: ob-http" \
+    --header "Authorization: Bearer <YOUR_ACCESS_TOKEN_HERE>" \
