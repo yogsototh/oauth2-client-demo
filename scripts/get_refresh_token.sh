161002ee63e11a9205fb3f1a0575f6d942b28c4d
Leland Krych <lkrych@cisco.com>
2020-03-04 15:59:45 -0800

    use cl params

--- scripts/get_refresh_token.sh
+++ scripts/get_refresh_token.sh
@@ -1,9 +1,12 @@
+AUTH=`echo -n "$1:$2" | base64`
+
 curl -X POST https://visibility.amp.cisco.com/iroh/oauth2/token \
+    --http1.1 \
     --header "Accept: application/json" \
     --header "Content-Type: application/x-www-form-urlencoded; charset=UTF-8" \
     --header "User-Agent: ob-http" \
-    --header "Authorization: Basic <BASE64 ENCODING OF CLIENT_ID:CLIENT_SECRET >" \
-    -d "code=<CODE JWT RETURNED FROM CTR>" \
+    --header "Authorization: Basic $AUTH" \
+-d "code=<CODE JWT RETURNED FROM CTR>" \
     -d "redirect_uri=https://localhost:4443/callback" \
     -d "scope=admin" \
     -d "grant_type=authorization_code"
