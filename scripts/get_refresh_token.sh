194ca5f053d139de22695e142856d034d7a550f9
Leland Krych <lkrych@cisco.com>
2020-03-04 16:03:56 -0800

    put things back in the right place

--- scripts/get_refresh_token.sh
+++ scripts/get_refresh_token.sh
@@ -6,7 +6,7 @@ curl -X POST https://visibility.amp.cisco.com/iroh/oauth2/token \
     --header "Content-Type: application/x-www-form-urlencoded; charset=UTF-8" \
     --header "User-Agent: ob-http" \
     --header "Authorization: Basic $AUTH" \
--d "code=<CODE JWT RETURNED FROM CTR>" \
+    -d "code=<CODE JWT RETURNED FROM CTR>" \
     -d "redirect_uri=https://localhost:4443/callback" \
     -d "scope=admin" \
     -d "grant_type=authorization_code"
