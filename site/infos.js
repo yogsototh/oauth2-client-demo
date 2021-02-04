56aca4c0cfc23673a73f937905e784af324f2627
Yann Esposito (Yogsototh) <yann.esposito@gmail.com>
2021-02-04 18:45:38 +0100

    add a posture demo

--- site/infos.js
+++ site/infos.js
@@ -1,4 +1,4 @@
-var clientname="localtest";
+var clientname="int-posture";
 
 var redirect_uri="https://localhost:5443/callback";
 
@@ -23,6 +23,13 @@ switch (clientname) {
       client_id="client-92258bc0-196a-4f6c-a0b5-fe105de5f505";
       client_password = "xoM2Gisjqp7Sp1jnw9Y6Yce9gfg4hmoRxZTekLqKi6i7o1xBiPvLvA";
       scopes=[ "openid" ];
+      break;
+    case "int-posture":
+      oauthURLPrefix="https://visibility.int.iroh.site";
+      response_type="code";
+      client_id="client-7230cfcf-648e-4b95-b85f-f73fe33fa0c3";
+      client_password = "vC9S-hvbBxQtbNQ6h__IekcFb7k9pTJuIbUKHH1rEQUgvkjpGjExhw";
+      scopes=[ "openid", "profile" ];
       break;
     default:
       alert("I don't know your client.");
