3529cc6c11886d62ad985e7a7ee56d5dd7e417b0
Olivier Barbeau - Cisco <obarbeau@cisco.com>
2021-11-04 12:04:00 +0100

    fix certfile name; serve from 'site' subdir

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
 import http.server, ssl
 
+class Handler(http.server.SimpleHTTPRequestHandler):
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, directory="site", **kwargs)
+
 server_address = ('localhost', 4443)
-httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
+httpd = http.server.HTTPServer(server_address, Handler)
 httpd.socket = ssl.wrap_socket(httpd.socket,
                                server_side=True,
-                               certfile='cert/server.csr',
+                               certfile='cert/server.crt',
                                keyfile='cert/server.key',
                                ssl_version=ssl.PROTOCOL_TLS)
 print("serving demo on port 4443")
